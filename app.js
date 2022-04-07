ec6410a389a12752499f9d08c7ad2efbc094bfc7
Nandni Asati <66835772+nandni25@users.noreply.github.com>
2022-04-08 01:01:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.querySelector('.close').style.display='none';
+document.querySelector('.ham').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+    if(    document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.close').style.display='none'
+
+    }
+    else{
+    document.querySelector('.ham').style.display='inline'
+        document.querySelector('.close').style.display='none'
+    }
+})
